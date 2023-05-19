90e9ca8e5361423164732aebcab26961b4712ccc
Diego Paixão <diego.spaixao91@hotmail.com>
2023-05-19 20:41:48 -0300

    Adiciona melhorias à calculadora
    Adiciona função adicionarPorcentagem() para calcular a porcentagem de um valor.
    Utiliza o método math.evaluate() para avaliar as expressões matemáticas de forma segura.
    Implementa validação para evitar a repetição de operadores consecutivos.
    Corrige a exibição do resultado ao pressionar o botão de igual.
    Aprimora a responsividade da calculadora para dispositivos de tela pequena.

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,38 +1,58 @@
 function iniciarCalculadora() {
     document.getElementById('display').value = '0';
-}
-
-function adicionarAoDisplay(valor) {
+  }
+  
+  function adicionarAoDisplay(valor) {
     var display = document.getElementById('display');
     var displayValue = display.value;
-
+  
+    // Verificar se o último caractere no display é um operador
+    var ultimoCaractere = displayValue.slice(-1);
+    var operadores = ['+', '-', '*', '/'];
+    if (operadores.includes(ultimoCaractere) && operadores.includes(valor)) {
+      // Substituir o operador anterior pelo novo operador
+      display.value = displayValue.slice(0, -1) + valor;
+    } else {
+      // Adicionar o valor normalmente
+      display.value = displayValue + valor;
+    }
+  
     // Remover o zero inicial ao digitar um valor
     if (displayValue === '0') {
-        displayValue = '';
+      display.value = valor;
     }
-
-    display.value = displayValue + valor;
-}
-
-function limparDisplay() {
+  }
+  
+  function limparDisplay() {
     document.getElementById('display').value = '0';
-}
-
-function retroceder() {
+  }
+  
+  function retroceder() {
     var valorDisplay = document.getElementById('display').value;
     document.getElementById('display').value = valorDisplay.slice(0, -1);
-}
-
-function calcular() {
+  }
+  
+  function calcular() {
     var valorDisplay = document.getElementById('display').value;
-    var resultado = eval(valorDisplay);
+    var resultado;
+  
+    try {
+      resultado = math.evaluate(valorDisplay);
+    } catch (error) {
+      // Lidar com erros de expressões inválidas
+      resultado = 'Erro';
+    }
+  
     document.getElementById('display').value = resultado;
-}
-
-function adicionarPorcentagem() {
+  }
+  
+  function adicionarPorcentagem() {
     var valorDisplay = document.getElementById('display').value;
     var valorNumerico = parseFloat(valorDisplay);
     var porcentagem = valorNumerico / 100;
     var resultado = porcentagem.toFixed(2); // Limita o resultado a duas casas decimais
     document.getElementById('display').value = resultado;
-}
+  }
+  
+  iniciarCalculadora();
+  
